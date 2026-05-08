fcb8299d4c5cb4e36f5ade57c25372d2fb6dea59
SonaAlieva <nailyasona@icloud.com>
2026-05-08 21:49:23 +0300

    Add files via upload

---
+++ Lab1/Palindrome.java
@@ -0,0 +1,28 @@
+public class Palindrome {
+    public static void main(String[] args){
+        for (int i = 0; i < args.length; i++){
+            String s = args[i];
+            if (isPalindrome(s)) {
+                System.out.println(s + " - палиндром");
+            } else {
+                System.out.println(s + " - не палиндром");
+            }
+
+        }
+
+    }
+
+    public static String reverseString(String s){
+        String reverse = "";
+        for (int i = s.length() - 1; i >= 0; i--){
+            reverse += s.charAt(i);
+        }
+        return reverse;
+
+    }
+
+    public static boolean isPalindrome(String s){
+        return s.equals(reverseString(s));
+    }
+
+}
