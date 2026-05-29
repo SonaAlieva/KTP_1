ceb6f9b3c0a3ed34248f368df2f8cc733ddd0f3a
SonaAlieva <nailyasona@icloud.com>
2026-05-29 16:08:52 +0300

    Update Store.java

--- Lab3/Store.java
+++ Lab3/Store.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class Store {
 
-    // HashMap: артикул  товар
+   
     private HashMap<String, Product> products = new HashMap<>();
 
 
@@ -20,33 +20,33 @@ public class Store {
         products.remove(article);
     }
 
-    // Показать все товары
+    
     public void printAllProducts() {
         for (String key : products.keySet()) {
             System.out.println(key + " → " + products.get(key));
         }
     }
 
-    // Тест
+   
     public static void main(String[] args) {
         Store store = new Store();
 
-        // создаём товары
+        
         Product p1 = new Product("Laptop", "Gaming laptop", 1200.0, 5);
         Product p2 = new Product("Phone", "Smartphone", 800.0, 10);
 
-        // добавляем
+       
         store.addProduct("A123", p1);
         store.addProduct("B456", p2);
 
-        // поиск
+        
         System.out.println("Поиск A123:");
         System.out.println(store.getProduct("A123"));
 
-        // удаление
+       
         store.removeProduct("B456");
 
-        // вывод всех
+        
         System.out.println("Все товары");
         store.printAllProducts();
     }
